1c619a42918f4775e2fe4cddc024a4c0cc2dcdba
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-09-10 00:47:52 +0300

    Added support of doubles in expression

--- calculator/src/main/scala/ru/spbau/bogomolov/scala/calculator/tokens/Number.scala
+++ calculator/src/main/scala/ru/spbau/bogomolov/scala/calculator/tokens/Number.scala
@@ -1,20 +1,29 @@
 package ru.spbau.bogomolov.scala.calculator.tokens
 
 class Number extends Token {
-  private[this] var value$ = 0
+  private[this] var value$: Double = 0
 
-  def value: Int = value$
+  def value: Double = value$
 
   override def parseFrom(string: String) : ParseResult = {
     if (string.charAt(0).isDigit) {
-      var position = 0
-      while(position < string.length && string.charAt(position).isDigit) {
+      var position = getEndPositionOfNumber(string)
+      if (position < string.length && string.charAt(position) == '.') {
         position += 1
+        position += getEndPositionOfNumber(string.substring(position))
       }
-      value$ = Integer.parseInt(string.substring(0, position))
+      value$ = string.substring(0, position).toDouble
       return ParseResult(this, position)
     }
     return ParseResult(null, 0)
+  }
+
+  private def getEndPositionOfNumber(string: String) : Int = {
+    var position = 0
+    while(position < string.length && string.charAt(position).isDigit) {
+      position += 1
+    }
+    return position
   }
 }
 
