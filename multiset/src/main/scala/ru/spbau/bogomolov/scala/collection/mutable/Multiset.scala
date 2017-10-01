9bc4c62c3fdf71a889cca4b78479261df62b1a6d
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2017-10-02 01:04:55 +0300

    Added foreach and filter.

--- multiset/src/main/scala/ru/spbau/bogomolov/scala/collection/mutable/Multiset.scala
+++ multiset/src/main/scala/ru/spbau/bogomolov/scala/collection/mutable/Multiset.scala
@@ -9,13 +9,38 @@ import scala.collection.mutable
 class Multiset[A] {
   private val elementCount = mutable.HashMap.empty[A, Int]
 
+  /**
+    * Add "element" to the set "count" times. By default add 1 time.
+    */
   def add(element: A, count: Int = 1): Unit = {
     if (count <= 0) throw new IllegalArgumentException("add should be called with positive count.")
     val currentCount = elementCount.getOrElse(element, 0)
     elementCount.update(element, currentCount + count)
   }
 
+  /**
+    * If element is presented in the multiset then returns Option with it else an empty Option.
+    */
   def find(element: A): Option[A] = elementCount.get(element).flatMap(_ => Some(element))
 
+  /**
+    * Returns count of element in the multiset.
+    */
   def count(element: A): Int = elementCount.getOrElse(element, 0)
+
+  /**
+    * Calls function on each element of the set. Takes into account count of element.
+   */
+  def foreach(func: (A, Int) => Unit): Unit = {
+    for ((element, count) <- elementCount) func(element, count)
+  }
+
+  /**
+    * Returns another multiset that consists of elements that predicate accepts.
+    */
+  def filter(predicate: A => Boolean): Multiset[A] = {
+    val filtered = new Multiset[A]
+    foreach((element, count) => if (predicate(element)) filtered.add(element, count))
+    filtered
+  }
 }
--- multiset/src/test/scala/ru/spbau/bogomolov/scala/collection/mutable/MultisetTest.scala
+++ multiset/src/test/scala/ru/spbau/bogomolov/scala/collection/mutable/MultisetTest.scala
@@ -6,27 +6,50 @@ class MultisetTest extends FunSuite {
 
   val value = 3
   val count = 7
+  val limit = 9
 
   test("testEmptyGet") {
-    val multiset = new Multiset[Int]()
+    val multiset = new Multiset[Int]
     assert(multiset.find(value).isEmpty)
   }
 
   test("testEmptyCount") {
-    val multiset = new Multiset[Int]()
+    val multiset = new Multiset[Int]
     assert(multiset.count(value) == 0)
   }
 
   test("testAddGet") {
-    val multiset = new Multiset[Int]()
+    val multiset = new Multiset[Int]
     multiset.add(value)
     val findResult = multiset.find(value)
     assert(findResult.isDefined && findResult.get == value)
   }
 
   test("testAddCount") {
-    val multiset = new Multiset[Int]()
+    val multiset = new Multiset[Int]
     multiset.add(value, count)
     assert(multiset.count(value) == count)
+  }
+
+  test("testForEach") {
+    val multiset = new Multiset[Int]
+    var counter = 0
+    for (i <- 1 to value) {
+      multiset.add(i, count)
+    }
+    multiset.foreach((value, count) => counter += value * count)
+    assert(counter == count * value * (value + 1) / 2)
+  }
+
+  test("testFilter") {
+    val multiset = new Multiset[Int]
+    for (i <- 1 to limit) {
+      multiset.add(i)
+    }
+    val filtered = multiset.filter((value) => value % 2 == 0)
+    for (i <- 1 to limit) {
+      if (i % 2 == 0) assert(filtered.count(i) == 1)
+      else assert(filtered.count(i) == 0)
+    }
   }
 }
